f6dbfeb4b98ce7560125d38abcd9c0e386ad1e2f
Vetreria <90621103+Vetreria@users.noreply.github.com>
2023-02-22 03:01:24 +0300

    optimize comments count evaluation.

--- blog/views.py
+++ blog/views.py
@@ -3,44 +3,57 @@ from blog.models import Comment, Post, Tag
 from django.db.models import Count
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
+# def get_related_posts_count(tag):
+#     return tag.posts.count()
 
 
-def serialize_post(post):
+def serialize_post_optimized(post):
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
         'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
+        'comments_amount': post.comments.count(),
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.prefetch_related("posts")], #.prefetch_related('posts')],
         'first_tag_title': post.tags.all()[0].title,
     }
+
+
+# def serialize_post(post):
+#     return {
+#         'title': post.title,
+#         'teaser_text': post.text[:200],
+#         'author': post.author.username,
+#         'comments_amount': Comment.objects.filter(post=post).count(),
+#         'image_url': post.image.url if post.image else None,
+#         'published_at': post.published_at,
+#         'slug': post.slug,
+#         'tags': [serialize_tag(tag) for tag in post.tags.all()],
+#         'first_tag_title': post.tags.all()[0].title,
+#     }
 
 
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': tag.posts.count(),
     }
 
 
 def index(request):
     most_popular_posts = Post.objects.prefetch_related('author').annotate(num_likes=Count('likes')).order_by('-num_likes')[:5]
+    most_fresh_posts = Post.objects.prefetch_related('author').order_by('published_at')[:5]
 
-    fresh_posts = Post.objects.prefetch_related('author').order_by('published_at')
-    most_fresh_posts = list(fresh_posts)[-5:]
 
-    most_popular_tags = Tag.objects.annotate(num_posts=Count('posts')).order_by('-num_posts')[:5]
+    most_popular_tags = Tag.objects.prefetch_related('posts').annotate(num_posts=Count('posts')).order_by('-num_posts')[:5]
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
@@ -57,7 +70,7 @@ def post_detail(request, slug):
             'author': comment.author.username,
         })
 
-    likes = post.likes.all()
+    # likes = post.likes.all()
 
     related_tags = post.tags.all()
 
@@ -66,16 +79,16 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': len(likes),
+        'likes_amount': post.likes.count(),
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
         'tags': [serialize_tag(tag) for tag in related_tags],
     }
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    # all_tags = Tag.objects.all()
+    # popular_tags = sorted(all_tags, key=get_related_posts_count)
+    most_popular_tags = Tag.objects.all()[:5]
 
     most_popular_posts = []  # TODO. Как это посчитать?
 
@@ -83,7 +96,7 @@ def post_detail(request, slug):
         'post': serialized_post,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
     }
     return render(request, 'post-details.html', context)
@@ -93,8 +106,8 @@ def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
     all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    popular_tags = Tag.objects.annotate(num_posts=Count('posts')).order_by('-num_posts')[:5]
+    most_popular_tags = Tag.objects.all()[:5]
 
     most_popular_posts = []  # TODO. Как это посчитать?
 
@@ -103,9 +116,9 @@ def tag_filter(request, tag_title):
     context = {
         'tag': tag.title,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
-        'posts': [serialize_post(post) for post in related_posts],
+        'posts': [serialize_post_optimized(post) for post in related_posts],
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
     }
     return render(request, 'posts-list.html', context)
